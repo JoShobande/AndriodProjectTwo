21a0981a5fe0ae8f30c66f704d0bd8bdbd52532b
Abdulrasheed-CDR <omogbolahanyahya14@gmail.com>
2025-04-06 21:46:57 -0400

    MainActivitytest

---
+++ BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityTest.java
@@ -0,0 +1,22 @@
+package com.example.android.testing.espresso.BasicSample;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+}
